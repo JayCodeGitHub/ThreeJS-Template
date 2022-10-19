ec69b1a614d2e3c216e31ca9d392f9b4026e856f
JayCodeGitHub <JayCode.contakt@gmail.com>
2022-10-20 01:40:11 +0200

    added scene camera and renderer

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,9 @@
-import { camelCase } from 'lodash'
 import '../styles/index.scss'
+import * as THREE from 'three';
 
-console.log(camelCase('test'));
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window,innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
